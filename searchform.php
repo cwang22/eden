8967e473609ee3ab844bf2aaf5288a6131e6ae0b
cwang22 <wcg0304@sina.com>
2015-11-06 17:46:41 +1100

    update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,5 +1,4 @@
 <form role="search" method="get" class="search-form form-inline" action="<?php echo home_url( '/' ); ?>">
-        <input type="search" class="search form-control" placeholder="<?php echo esc_attr_x( 'Search', 'eden' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
-        <input type="submit" class="btn btn-primary" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
-    
+    <input type="search" class="form-control input-search" placeholder="<?php echo esc_attr_x( 'Search', 'eden' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+    <input type="submit" class="btn btn-primary btn-search" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
 </form>
