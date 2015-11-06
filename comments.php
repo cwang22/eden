287fd00ab4e37cbba00250ec8b52b6bb744b266a
cwang22 <wcg0304@sina.com>
2015-11-06 17:43:52 +1100

    update comments.php.

--- comments.php
+++ comments.php
@@ -27,15 +27,11 @@ if ( post_password_required() ) {
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-				printf( // WPCS: XSS OK.
-					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'eden' ) ),
-					number_format_i18n( get_comments_number() ),
-					'<span>' . get_the_title() . '</span>'
-				);
+				printf( _e( 'Comments', 'eden' ));
 			?>
 		</h2>
-
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+
 		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
 			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'eden' ); ?></h2>
 			<div class="nav-links">
@@ -45,18 +41,19 @@ if ( post_password_required() ) {
 
 			</div><!-- .nav-links -->
 		</nav><!-- #comment-nav-above -->
+
 		<?php endif; // Check for comment navigation. ?>
 
 		<ol class="comment-list">
 			<?php
 				wp_list_comments( array(
-					'style'      => 'ol',
-					'short_ping' => true,
+					'callback' => 'eden_comment'
 				) );
 			?>
 		</ol><!-- .comment-list -->
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+
 		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
 			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'eden' ); ?></h2>
 			<div class="nav-links">
@@ -66,6 +63,7 @@ if ( post_password_required() ) {
 
 			</div><!-- .nav-links -->
 		</nav><!-- #comment-nav-below -->
+		
 		<?php endif; // Check for comment navigation. ?>
 
 	<?php endif; // Check for have_comments(). ?>
@@ -76,7 +74,25 @@ if ( post_password_required() ) {
 	?>
 		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'eden' ); ?></p>
 	<?php endif; ?>
-
-	<?php comment_form(); ?>
-
+	
+	<?php
+	$args = array(
+		'fields' => apply_filters(
+			'comment_form_default_fields', array(
+			'author' => '<div class="form-group"><input id="author" class="form-control" placeholder="Name" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' ></div>',
+			'email'  => '<div class="form-group"><input id="email" class="form-control" placeholder="Email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .'" size="30"' . $aria_req . '></div>',
+			'url'    => '<div class="form-group"><input id="url" class="form-control" placeholder="Website" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30"></div>'
+			)
+		),
+		'comment_field' => '<div class="form-group"><textarea id="comment" class="form-control" name="comment" placeholder="Comment" cols="45" rows="8" aria-required="true"></textarea></div>',
+		'comment_notes_before' => '',
+		'class_submit' => 'btn btn-primary pull-right'
+	);
+	?>
+	
+	<div class="row">
+		<div class="col-sm-8 col-xs-12">
+			<?php comment_form( $args ); ?>
+		</div>
+	</div>
 </div><!-- #comments -->
