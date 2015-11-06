cf6874efd709c2fb6f8aac154ba5c0211e308a0f
cwang22 <wcg0304@sina.com>
2015-11-06 22:24:54 +1100

    add page-header class

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ get_header(); ?>
 					<?php if ( have_posts() ) : ?>
 
 						<?php if ( is_home() && ! is_front_page() ) : ?>
-							<header>
+							<header class="page-header">
 								<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
 							</header>
 						<?php endif; ?>
